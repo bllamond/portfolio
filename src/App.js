000daf34633f78bb4328d5a3fe1e0a92f5107df9
bllamond <vinaymalik1729@gmail.com>
2024-07-13 22:25:59 +0530

    update notfound

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 				<Route path="/about" element={<About />} />
 				<Route path="/projects" element={<Projects />} />
 				<Route path="/contact" element={<Contact />} />
-				<Route path="*" element={<Notfound />} />
+				<Route path="*" element={<Homepage />} />
 			</Routes>
 		</div>
 	);
